8dc667c86db5329f6f605c81243b1783a8bac15c
vitalii.hodzevych <vitalii.hodzevych@dev.pro>
2020-10-26 14:13:48 +0200

    add solutions for api design course

--- api design in node/src/utils/__tests__/crud.spec.js
+++ api design in node/src/utils/__tests__/crud.spec.js
@@ -96,6 +96,7 @@ describe('crud controllers', () => {
   describe('createOne', () => {
     test('creates a new doc', async () => {
       expect.assertions(2)
+
       const user = mongoose.Types.ObjectId()
       const body = { name: 'name' }
 
@@ -119,6 +120,7 @@ describe('crud controllers', () => {
 
     test('createdBy should be the authenticated user', async () => {
       expect.assertions(2)
+
       const user = mongoose.Types.ObjectId()
       const body = { name: 'name' }
 
